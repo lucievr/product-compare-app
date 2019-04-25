192d9cb6881d23cecfe10cb9b3adde2614421a49
Lucie <46979603+lucievr@users.noreply.github.com>
2019-04-25 20:09:56 +0200

    Restructure directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,55 @@
 import React from "react"
 import { Link } from "gatsby"
 // import
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import "../components/layout.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    )}
+  />
 )
+
+IndexPage.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export default IndexPage
